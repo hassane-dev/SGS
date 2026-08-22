dc77e5d2b2af06218d4117f788fda52b1ae5b410
hassane-dev <84070420+hassane-dev@users.noreply.github.com>
2026-08-22 12:04:03 +0100

    Revert "Fix Paie Bulletins preparation view rendering and sidebar route matching"

--- src/controllers/PaieBulletinsController.php
+++ src/controllers/PaieBulletinsController.php
@@ -35,7 +35,7 @@ class PaieBulletinsController {
 
         $selectedPeriode = $periodeId ? PaiePeriode::findById($periodeId) : null;
         $eligibleContracts = $periodeId ? PersonnelContractService::getEligibleContractsForPeriod($periodeId, $lyceeId) : [];
-        $cycles = Cycle::findByLycee($lyceeId);
+        $cycles = Cycle::findAllForLycee($lyceeId);
 
         include __DIR__ . '/../views/paie/bulletins/prepare.php';
     }
@@ -229,7 +229,7 @@ class PaieBulletinsController {
         $reglesSnapshot = PaieBulletinRegleSnapshot::findByBulletinId($id);
 
         $lyceeId = Auth::getLyceeId() ?: 1;
-        $comptesFinanciers = CompteFinancier::findByLycee($lyceeId);
+        $comptesFinanciers = CompteFinancier::findAllForLycee($lyceeId);
 
         include __DIR__ . '/../views/paie/bulletins/show.php';
     }
